a0c9035a78bc62f07bd874df75bd0ce3747e7dfa
mellow0412 <gautham@stylabs.in>
2024-09-04 16:55:13 +0530

    mongo uri

--- server/database/db.js
+++ server/database/db.js
@@ -3,15 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const Connection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then((data) => {
-      console.log(`Connected to ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log("Error while connecting to database", err.message);
-    });
+const Connection = async () => {
+  const MONGODB_URI = process.env.MONGO_URI;
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error while connecting to the database:", error.message);
+  }
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected");
+  });
+
+  mongoose.connection.on("error", (error) => {
+    console.log("Error in the connection:", error.message);
+  });
 };
 
 export default Connection;
